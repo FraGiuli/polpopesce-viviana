61bbeecd9f33c489856091657a6828db313ee73e
francescoguerrini <francescoguerrini1987@gmail.com>
2024-09-16 13:54:16 +0200

    cookies polpo

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,4 +1,5 @@
 import Text from "./Text/Text";
+import Script from "next/script";
 
 export default function Footer() {
   return (
@@ -10,7 +11,55 @@ export default function Footer() {
         <Text textAlign="center" color="white">
           © 2023 ALICETTE S.R.L. P.IVA 08390270968
         </Text>
+        <div className="flex gap-1">
+          {/* Link alla Privacy Policy */}
+          <a
+            href="https://www.iubenda.com/privacy-policy/39911099"
+            className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe text-white hover:text-slate-300"
+            title="Privacy Policy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Privacy Policy
+          </a>
+          <span className="text-white text-xl">|</span>
+
+          {/* Link alla Cookie Policy */}
+          <a
+            href="https://www.iubenda.com/privacy-policy/39911099/cookie-policy"
+            className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe text-white hover:text-slate-300"
+            title="Cookie Policy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cookie Policy
+          </a>
+        </div>
       </div>
+
+      {/* Caricamento dello script di Iubenda */}
+      <Script
+        id="iubenda-cookie-policy-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function (w,d) {
+              var loader = function () {
+                var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0];
+                s.src="https://cdn.iubenda.com/iubenda.js";
+                tag.parentNode.insertBefore(s,tag);
+              };
+              if(w.addEventListener){
+                w.addEventListener("load", loader, false);
+              } else if(w.attachEvent) {
+                w.attachEvent("onload", loader);
+              } else {
+                w.onload = loader;
+              }
+            })(window, document);
+          `,
+        }}
+      />
     </div>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 import AOS from "aos";
 import { useEffect } from "react";
+import Script from "next/script"; // Importa il componente Script
 
 export default function App({ Component, pageProps }) {
   const theme = createTheme({
     type: "light", // it could be "light" or "dark"
   });
+
   useEffect(() => {
     AOS.init();
   }, []);
@@ -16,6 +18,33 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <NextUIProvider theme={theme}>
+        {/* Configurazione di Iubenda */}
+        <Script id="iubenda-config" strategy="beforeInteractive">
+          {`
+            var _iub = _iub || [];
+            _iub.csConfiguration = {
+              "siteId": 3761588,
+              "cookiePolicyId": 39911099,
+              "lang": "it"
+            };
+          `}
+        </Script>
+
+        {/* Script di autoblocco Iubenda */}
+        <Script
+          strategy="beforeInteractive"
+          src="https://cs.iubenda.com/autoblocking/3761588.js"
+        />
+
+        {/* Script principale Iubenda */}
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.iubenda.com/cs/iubenda_cs.js"
+          charSet="UTF-8"
+          async
+        />
+
+        {/* Il resto della tua app */}
         <Component {...pageProps} />
       </NextUIProvider>
     </Provider>
